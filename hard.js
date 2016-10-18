07255f92e052963d75eff9ead28eeed2169241ed
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-10-17 19:44:21 -0700

    added fade in and fade out for the hex clock. Also cleaned up css for jsClock

--- hard.js
+++ hard.js
@@ -1,32 +1,32 @@
 
-//jquery.css(`"background". ${hexcode}`))
 
-var hexadecimals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
-
-
-hexValues()
-
-randHexValues()
+$(document).ready(function() {
 
 
-function hexValues() {
-	hexNumber = "#"
-	for (var i = 0; i < 6; i += 1) {
-	hexNumber += hexadecimals[Math.floor(Math.random() * 16)]
-	$("#hex").html(hexNumber)
-	$("body").css("background",hexNumber)
-	}
-}
+	var hexadecimals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
-function randHexValues() {
+	var hexNumber = "#"
+
+	randHexValue()
+
 	setInterval(function() {
-		hexValues()
-	}, 3000)	
-}
+		randHexValue()
+		$("#hex1").toggleClass("current")
+		$("#hex2").toggleClass("current")	
+	}, 3000)
 
 
+	
+	function randHexValue() {
+		for (var i = 0; i < 6; i += 1) {
+				hexNumber += hexadecimals[Math.floor(Math.random() * 16)]
+		}
+		
+		$("#hex1, #hex2").html(hexNumber).css("background", hexNumber)
+		console.log(hexNumber)
+		hexNumber = "#"
+	}
+
+})
 
 
-// $("body").fadeOut("slow", function() {
-// 		$(this).css("background",hexNumber)
-// 	})
